68aafc9559cc535ce2bf80779ea861525b86b4a7
Jake <Jake_Guy_11@protonmail.ch>
2021-11-29 17:36:48 -0500

    added file content fetching

--- app/src/main/java/com/jakeguy11/VCast/MainActivity.java
+++ app/src/main/java/com/jakeguy11/VCast/MainActivity.java
@@ -16,6 +16,7 @@ import android.widget.TextView;
 
 import org.json.*;
 
+import java.io.BufferedInputStream;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileInputStream;
@@ -23,11 +24,16 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
 import java.net.URL;
 import java.nio.charset.StandardCharsets;
 
 public class MainActivity extends AppCompatActivity
 {
+
+    // Some Strings we'll need
+    private String configFilePath = "";
+    private String remoteJsonUrl = "https://raw.githubusercontent.com/JakeGuy11/VCast/main/server_data/dummy_config.json";
 
     // Do all the initialization stuff
     @Override
@@ -44,22 +50,52 @@ public class MainActivity extends AppCompatActivity
             actionBar.setBackgroundDrawable(new ColorDrawable(Color.parseColor(getResources().getString((int) R.color.pri_blue))));
         }
 
-        // For testing below here
-        // Inject the dummy entry
-        LayoutInflater inflater = (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        // Test out the getFileContentFromURL
+        String res = getFileContentFromURL(remoteJsonUrl);
+        System.out.println("Printing response:");
+        System.out.println(res);
 
-        String[][] members = {{"Polka", "https://vignette.wikia.nocookie.net/virtualyoutuber/images/3/33/Omaru_Polka_Portrait.png/revision/latest?cb=20200807015145"}, {"Towa", "https://static.wikia.nocookie.net/virtualyoutuber/images/3/35/Tokoyami_Towa_-_Portrait.png/revision/latest?cb=20200126194247"}, {"Suisei", "https://static.wikia.nocookie.net/virtualyoutuber/images/8/8b/Hoshimachi_Suisei_2019_Portrait.png/revision/latest?cb=20191205132210"}, {"Mori", "https://static.wikia.nocookie.net/virtualyoutuber/images/3/39/Mori_Calliope_Portrait.png/revision/latest?cb=20200910193007"}};
-
-        for(String[] currentMem : members)
+        // Check if the user has a config - if they don't, download it
+        if (!fileExists(getApplicationContext(), configFilePath))
         {
-            // Create the view, edit the properties
-            android.view.View viewToAdd = inflater.inflate(R.layout.entry, null);
-            ((ImageView) viewToAdd.findViewById(R.id.sample_image)).setImageDrawable(loadImageFromWeb(currentMem[1]));
-            ((TextView) viewToAdd.findViewById(R.id.sample_text)).setText(currentMem[0]);
+            // Download the file
 
-            // Add the view
-            ((LinearLayout) findViewById(R.id.live_content_container)).addView(viewToAdd);
         }
+    }
+
+    public static String getFileContentFromURL(String providedUrl)
+    {
+        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+        StrictMode.setThreadPolicy(policy);
+
+        HttpURLConnection urlConnection;
+        String returnStr = "";
+        try
+        {
+            // Open the URL, make the call
+            URL conUrl = new URL(providedUrl);
+            urlConnection = (HttpURLConnection) conUrl.openConnection();
+            int callResponse = urlConnection.getResponseCode();
+
+            if (callResponse == 200)
+            {
+                // Everything went well, get the response
+                InputStream responseStream = new BufferedInputStream(urlConnection.getInputStream());
+                if (responseStream != null)
+                {
+                    // There's actually something in the response
+                    BufferedReader responseReader = new BufferedReader(new InputStreamReader(responseStream));
+
+                    // Iterate through the lines of the response, appending them to the string to return
+                    String currentLine = "";
+                    while ((currentLine = responseReader.readLine()) != null) returnStr += currentLine;
+                }
+                responseStream.close();
+                return returnStr;
+            }
+        }
+        catch (Exception e) { System.out.println(e); }
+        return null;
     }
 
     // Turn a URL into a drawable image
